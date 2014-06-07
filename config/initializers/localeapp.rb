e5b42192d649f76a244500c448ddde27df1c65c2
Jerome Lefeuvre <jerome.lefeuvre@gmail.com>
2014-06-07 18:41:57 -0400

    Add line for not pushing to localeapp everytime

--- config/initializers/localeapp.rb
+++ config/initializers/localeapp.rb
@@ -5,4 +5,5 @@ Localeapp.configure do |config|
   config.translation_data_directory = 'locales'
   config.synchronization_data_file  = '.localeapp/log.yml'
   config.daemon_pid_file            = '.localeapp/localeapp.pid'
+  # config.sending_environments       = []
 end
