a648bd5b9fe271daa5f80efbcbd0af3a8fbd3943
Jerome Lefeuvre <jerome.lefeuvre@gmail.com>
2014-06-07 19:01:54 -0400

    Validate title and price product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,7 @@
 class Product < ActiveRecord::Base
+  validates :title, :presence   => true,
+                    :uniqueness => true
+
+  validates :price, :numericality => true,
+                    :presence     => true,
 end
