4e754542289577bd0cb57e3302494e9fbbce3d52
Jerome Lefeuvre <jerome.lefeuvre@gmail.com>
2014-06-11 22:49:25 -0400

    Add small and icon version of Product Image

--- app/uploaders/product_image_uploader.rb
+++ app/uploaders/product_image_uploader.rb
@@ -4,7 +4,7 @@ class ProductImageUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -25,27 +25,29 @@ class ProductImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  process :scale => [200, 300]
-
-  def scale(width, height)
-    # do something
-  end
+  # process :scale => [200, 300]
+  #
+  # def scale(width, height)
+  #   # do something
+  # end
 
   # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process :resize_to_fit => [50, 50]
-  # end
+  def filename
+    "original.#{model.image.file.extension}" if original_filename
+  end
 
+  version :small do
+    process :resize_to_fit => [190, 190]
+    process :convert => 'png'
+  end
+
+  version :icon do
+    process :resize_to_fill => [50, 50]
+    process :convert => 'png'
+  end
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_white_list
-  #   %w(jpg jpeg gif png)
-  # end
-
-  # Override the filename of the uploaded files:
-  # Avoid using model.id or version_name here, see uploader/store.rb for details.
-  # def filename
-  #   "something.jpg" if original_filename
-  # end
-
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
 end
