81f6eeb766df6ad021b87dde8ab76d20d48ca98a
Jerome Lefeuvre <jerome.lefeuvre@gmail.com>
2014-06-23 16:31:51 -0400

    Improve products page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,20 +1,37 @@
 class ProductsController < ApplicationController
-  before_filter :set_product_categories
+  add_breadcrumb "Acceuil", :root_path
+  add_breadcrumb "Les produits", :products_path
 
   def index
-    @products = ProductCategory.where(id: params[:product_category_id]).try(:first).try(:products)
+    @products = if params[:product_category_id]
+      add_breadcrumb(
+        ProductCategory.find(params[:product_category_id]).title,
+        product_category_products_path(params[:product_category_id])
+      )
 
-    @products = Product.star if @products.blank?
+      ProductCategory.where(id: params[:product_category_id]).try(:first).try(:products)
+    else
+      add_breadcrumb(
+        'Coup de coeur'
+      )
+
+      Product.star
+    end
 
     @products = @products ? @products.active : []
   end
 
   def show
     @product = Product.find_by(id: params[:id], product_category_id: params[:product_category_id]) || not_found
+
+    add_breadcrumb(
+      @product.product_category.title,
+      product_category_products_path(@product.product_category)
+    )
+    add_breadcrumb(
+      @product.title,
+      [@product.product_category, @product]
+    )
   end
 
-  private
-  def set_product_categories
-    @product_categories = ProductCategory.only_with_products
-  end
 end
